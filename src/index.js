7e6e3aa6650ad35213907dfe0a3e96ba055e36ad
D-coder <chuksmbanasoj@gmail.com>
2022-02-15 04:30:23 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import "./index.css";
 import App from "./App";
 import { ReceipesProvider } from "./contexts/receipes_context";
 import "bootstrap/dist/css/bootstrap.css";
+import "aos/dist/aos.css";
+import "aos/dist/aos.js";
 
 ReactDOM.render(
   <React.StrictMode>
